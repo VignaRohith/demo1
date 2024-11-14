c26211186ae8271a42e4a4456e8d8dabf9b2a77c
VignaRohith <Govindarajuvigna_Rohith@epam.com>
2024-11-14 11:20:08 +0530

    solved new

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -17,39 +17,39 @@ import org.epam.openapi.App;
 import java.io.IOException;
 
 @LambdaHandler(lambdaName = "api_handler",
-		roleName = "api_handler-role",
-		layers = "sdk-layer",
+        roleName = "api_handler-role",
+        layers = "sdk-layer",
 		aliasName = "learn",
-		runtime = DeploymentRuntime.JAVA11,
-		architecture = Architecture.ARM64,
-		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        runtime = DeploymentRuntime.JAVA11,
+        architecture = Architecture.ARM64,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaLayer(
-		layerName = "sdk-layer",
-		libraries = {"lib/open-meteo-api-1.0-SNAPSHOT.jar"},
-		runtime = DeploymentRuntime.JAVA11,
-		architectures = {Architecture.ARM64},
-		artifactExtension = ArtifactExtension.ZIP
+        layerName = "sdk-layer",
+        libraries = {"lib/open-meteo-api-1.0-SNAPSHOT.jar"},
+        runtime = DeploymentRuntime.JAVA11,
+        architectures = {Architecture.ARM64},
+        artifactExtension = ArtifactExtension.ZIP
 )
 @LambdaUrlConfig(
-		authType = AuthType.NONE,
-		invokeMode = InvokeMode.BUFFERED
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
 )
 public class ApiHandler implements RequestHandler<Object, String> {
 
-	public String handleRequest(Object request, Context context) {
-		// Define latitude and longitude (can be parameterized based on incoming request)
-		App client = new App(50.4375, 30.5); // Using Kiev's coordinates
-		JsonObject weatherData = null;
+    public String handleRequest(Object request, Context context) {
+        // Define latitude and longitude (can be parameterized based on incoming request)
+        App client = new App(50.4375, 30.5); // Using Kiev's coordinates
+        JsonObject weatherData = null;
 
-		try {
-			// Fetch weather data
-			weatherData = client.getWeatherForecast();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+        try {
+            // Fetch weather data
+            weatherData = client.getWeatherForecast();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-		// Return the weather data in JSON format as a string
-		return weatherData.toString();
-	}
+        // Return the weather data in JSON format as a string
+        return weatherData.toString();
+    }
 }
